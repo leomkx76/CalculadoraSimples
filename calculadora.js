c45f3fd5f89a5dd11b2706e2aa78dc76acb584f9
LEONARDO ANTONIO GONCALVES <43411365+leomkx76@users.noreply.github.com>
2023-05-29 14:56:18 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,83 @@
+const teclasNum = [...document.querySelectorAll(".num")]
+const teclasOp = [...document.querySelectorAll(".op")]
+const teclaRes = document.querySelector(".res")
+const display = document.querySelector(".display")
+const ttonoff = document.querySelector(".tonoff")
+const tlimpar = document.querySelector("#tlimpar")
+const ttseparador = document.querySelector("#tseparador")
+const tigual = document.querySelector("#tigual")
+const tcopy = document.querySelector("#tcopy")
+const calc_aba = document.querySelector("#calc_aba")
+const calc = document.querySelector(".calc")
+const img_aba_calc = document.querySelector("#img_aba_calc")
+
+
+let sinal = false
+let decimal = false
+
+teclasNum.forEach((el)=>{
+    el.addEventListener("click", (evt)=>{
+        sinal = false
+        if(evt.target.innerHTML == ","){
+            if(!decimal){
+                decimal = true
+                if(display.innerHTML == "0"){
+                    display.innerHTML = "0,"
+                }else{
+                display.innerHTML+=evt.target.innerHTML
+                }
+            }
+        }else{
+            if(display.innerHTML == "0"){
+                display.innerHTML = ""
+            }
+                display.innerHTML+=evt.target.innerHTML
+        }
+    })
+})
+
+teclasOp.forEach((el)=>{
+    el.addEventListener("click", (evt)=>{
+        if(!sinal){
+            sinal = true
+            if(display.innerHTML == "0"){
+                display.innerHTML = ""
+            }
+            if(evt.target.innerHTML == "x"){
+                display.innerHTML+= "*"
+
+            }else{
+                display.innerHTML+=evt.target.innerHTML
+            }
+         
+        }
+
+    })
+})
+
+tlimpar.addEventListener("click", (evt)=>{
+    sinal = false
+    decimal = false
+    display.innerHTML = "0"
+})
+
+tigual.addEventListener("click", (evt)=>{
+    sinal = false
+    decimal = false
+    const res = eval(display.innerHTML)
+    display.innerHTML = res
+})
+
+tcopy.addEventListener("click", (evt)=> {
+    
+    navigator.clipboard.writeText(display.innerHTML)
+})
+
+calc_aba.addEventListener("click", (evt)=>{
+    calc.classList.toggle("calc_exibir")
+    if(calc.classList.contains("calc_exibir")){
+        img_aba_calc.setAttribute("src", "setaesquerda.png")
+    }else{
+        img_aba_calc.setAttribute("src", "./setadireita.png")
+    }
+})
